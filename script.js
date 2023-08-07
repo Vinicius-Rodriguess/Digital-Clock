6e4e82215f21ec6d38189312ea54577aa38ed926
Vinicius-Rodriguess <123024036+Vinicius-Rodriguess@users.noreply.github.com>
2023-08-07 11:23:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,17 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-const relogio = setInterval(function time(){
+const relogio = setInterval(() => {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let seg = dateToday.getSeconds();
 
     if (hr < 10) hr = '0' + hr;
-
     if (min < 10) min = '0' + min;
-
     if (seg < 10) seg = '0' + seg;
 
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = seg;
-})
+});
